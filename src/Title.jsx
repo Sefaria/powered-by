f55715ad23186895e4db52f2cf173fbd29a6b27d
saengel <sarah@sefaria.org>
2026-06-30 11:43:02 +0300

    customize template

---
+++ src/Title.jsx
@@ -0,0 +1,12 @@
+function Title() {
+  return (
+    <header>
+      <h1>Powered by Sefaria Dashboard</h1>
+      <a href="https://developers.sefaria.org" target="_blank" rel="noreferrer">
+        developers.sefaria.org
+      </a>
+    </header>
+  )
+}
+
+export default Title
