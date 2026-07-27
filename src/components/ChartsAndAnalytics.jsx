92ab576543050156006445c9b1a8ed011ec64ed2
Sefaria Intern <sefaria@Sefarias-MacBook-Air.local>
2026-07-27 10:40:17 +0300

    feat: add Charts and Analytics placeholder component

---
+++ src/components/ChartsAndAnalytics.jsx
@@ -0,0 +1,9 @@
+function ChartsAndAnalytics() {
+  return (
+    <div className="charts-and-analytics">
+      <p>Charts and analytics coming soon.</p>
+    </div>
+  )
+}
+
+export default ChartsAndAnalytics
