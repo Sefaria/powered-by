6906807eff0b676819a8725896ce928356ed2871
Sefaria Intern <sefaria@Sefarias-MacBook-Air.local>
2026-08-05 12:05:31 +0300

    refactor: rebuild KeywordFrequencyChart on generic BarChart

--- src/components/charts/KeywordFrequencyChart.jsx
+++ src/components/charts/KeywordFrequencyChart.jsx
@@ -1,19 +1,16 @@
-import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import BarChart from './types/BarChart.jsx'
 
 function KeywordFrequencyChart({ data }) {
   return (
-    <>
-      <h2>Keyword frequency</h2>
-      <ResponsiveContainer width="100%" height={data.length * 28 + 40}>
-        <BarChart data={data} layout="vertical" margin={{ left: 24 }}>
-          <CartesianGrid strokeDasharray="3 3" horizontal={false} />
-          <XAxis type="number" allowDecimals={false} />
-          <YAxis type="category" dataKey="keyword" width={100} />
-          <Tooltip />
-          <Bar dataKey="count" name="Projects" fill="var(--accent)" />
-        </BarChart>
-      </ResponsiveContainer>
-    </>
+    <BarChart
+      data={data}
+      title="Keyword frequency"
+      dataKey="count"
+      categoryKey="keyword"
+      layout="vertical"
+      categoryWidth={100}
+      height={data.length * 28 + 40}
+    />
   )
 }
 
